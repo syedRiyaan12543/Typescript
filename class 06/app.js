9cf9ff900454564c2e4782f3f279eef5751decc7
syedRiyaan12543 <160419157+syedRiyaan12543@users.noreply.github.com>
2024-03-21 16:16:19 +0500

    class 6 file update

--- class 06/app.js
+++ class 06/app.js
@@ -1,10 +1,32 @@
 "use strict";
 //class 6 start by revision of if else statements
-let email = "rayyan@gmail.com";
-let pass = '123456';
-if (email == "rayyan@gmail.com" && pass === "123456") {
-    console.log("Sucsessfully Logged In");
+// let email :string = "rayyan@gmail.com"
+// let pass :string = '123456'
+// if(email == "rayyan@gmail.com" && pass === "123456"){
+//     console.log("Sucsessfully Logged In");
+// }else{
+//     console.log("invalid Credentials");
+// }
+//else if statement
+let percentage = 100;
+if (percentage >= 80 && percentage <= 100) {
+    console.log("A+ Grade");
+}
+else if (percentage >= 70 && percentage <= 79.99) {
+    console.log("A Grade");
+}
+else if (percentage >= 60 && percentage <= 69.99) {
+    console.log("B Grade");
+}
+else if (percentage >= 50 && percentage <= 59.99) {
+    console.log("C Grade");
+}
+else if (percentage >= 33 && percentage <= 49.99) {
+    console.log("D Grade");
+}
+else if (percentage > 100) {
+    console.log("Invalid Percentage");
 }
 else {
-    console.log("invalid Credentials");
+    console.log("Fail");
 }
--- class 06/app.ts
+++ class 06/app.ts
@@ -1,10 +1,29 @@
 //class 6 start by revision of if else statements
 
-let email :string = "rayyan@gmail.com"
-let pass :string = '123456'
+// let email :string = "rayyan@gmail.com"
+// let pass :string = '123456'
 
-if(email == "rayyan@gmail.com" && pass === "123456"){
-    console.log("Sucsessfully Logged In");
-}else{
-    console.log("invalid Credentials");
+// if(email == "rayyan@gmail.com" && pass === "123456"){
+//     console.log("Sucsessfully Logged In");
+// }else{
+//     console.log("invalid Credentials");
+// }
+
+//else if statement
+let percentage =100
+
+if (percentage >=80 && percentage <=100){
+    console.log("A+ Grade");
+}else if(percentage >=70 && percentage <=79.99){
+    console.log("A Grade");
+}else if(percentage >=60 && percentage <=69.99){
+    console.log("B Grade");
+}else if(percentage >=50 && percentage <=59.99){
+    console.log("C Grade");
+}else if(percentage >=33 && percentage <=49.99){
+    console.log("D Grade");
+}else if(percentage > 100){
+    console.log("Invalid Percentage");
+}else {
+    console.log("Fail");
 }
